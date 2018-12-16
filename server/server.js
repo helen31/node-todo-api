17621febd6eebb5f2b2e7658d287d6fe0ed319a4
alena <a30058422@gmail.com>
2018-12-16 18:54:27 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 /**
  * Created by alena on 21/11/18.
  */
-var express = require('express');
-var bodyParser = require('body-parser');
+const _=require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const {ObjectID} = require('mongodb');
 
@@ -74,6 +75,33 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     })
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+       body.completedAt =  new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+            res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
