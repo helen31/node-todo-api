63e834007f45ee547caedfbfdb92db44264f494b
alena <a30058422@gmail.com>
2019-02-10 23:21:16 +0200

    Update config to use json file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,7 @@
  */
 
 var mongoose = require('mongoose');
+var config = require('../config/config');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
